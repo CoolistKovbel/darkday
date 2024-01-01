5515c00d194d1d4fbc0ab4dc93336cf12e31fe38
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-01-02 00:39:42 +0200

    yyyyy

--- app/api/featured/route.ts
+++ app/api/featured/route.ts
@@ -1,17 +1,21 @@
 import { db } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export async function get(req: NextRequest, res: NextResponse) {
-    try {
-        const data = await db.post.findMany({
-            where: {
-                featured: true
-            }
-        });
-
-        return NextResponse.json(data, { status: 200 });
-    } catch (error) {
-        console.error("Error:", error);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        try {
+            const data = await db.post.findMany({
+                where: {
+                    featured: true
+                }
+            });
+    
+            return res.status(200).json(data);
+        } catch (error) {
+            console.error("Error:", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    } else {
+        res.status(405).json({ error: "Method Not Allowed" });
     }
 }
