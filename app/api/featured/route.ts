6a5cf7f2e759dc9ac7daec1fb8d57b4aab1fd0ef
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-01-06 21:50:19 +0200

    maybe fix

--- app/api/featured/route.ts
+++ app/api/featured/route.ts
@@ -1,21 +1,18 @@
-
 import { db } from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-   
-        try {
-            const data = await db.post.findMany({
-                where: {
-                    featured: true
-                }
-            });
-    
-            return NextResponse.json(data, {status: 200});
-        } catch (error) {
-            console.error("Error:", error);
-            return res.json({ error: "Internal Server Error" , status: 500});
-        }
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await db.post.findMany({
+      where: {
+        featured: true,
+      },
+    });
 
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return res.json({ error: "Internal Server Error", status: 500 });
+  }
 }
